3ec9bebebf13f5ed486b61e3abb3aaa590cf559a
Luiz Rogerio Carvalho <luizrogeriocarvalho@gmail.com>
2019-02-02 15:25:28 -0200

    Fixed some command lines. Full working.

--- debootstrap/create.sh
+++ debootstrap/create.sh
@@ -4,27 +4,26 @@ SOURCE_URL=http://httpredir.debian.org/debian
 
 DOCKER_USER="lrgc01"
 
+DATE_STAMP=$(date +%m%d%H%M)
 # Min system (man debootstrap)
 VARIANT="minbase"
 
 if [ "$VARIANT" != "" ]; then
-   VARIANT_ARG="--variant=$VARIANT"
+   VARIANT_OPT="--variant=$VARIANT"
 fi
 
 # stretch, wheezy, stable, etc
 DISTRIB=stable
 
-if [ "$1" = "" ]; then
-  DEST_DIR=./build/${DISTRIB}-`date +%m%d%H%M` 
-else
-  DEST_DIR="$1"
-fi
+# You can pass DESTDIR via command argument
+DEST_DIR=${1:-"./${VARIANT}_${DISTRIB}-${DATE_STAMP}"}
+#DEST_DIR="minbase_stable-02021156"
 
-# Not used yet
-EXCLUDE="--exclude=openssh-client,ifupdown,iproute2,iptables,iputils-ping,isc-dhcp-client,e2fsprogs,dmidecode,ucf,vim-tiny"
+# Not necessary in most systems (no app will handle network admin for example - it's a dockerd task)
+EXCLUDE_OPT="--exclude=openssh-client,ifupdown,iproute2,iptables,iputils-ping,isc-dhcp-client,e2fsprogs,dmidecode,ucf,vim-tiny"
 
-# Create image (uncomment)
-####debootstrap $VARIANT_ARG $DISTRIB $DEST_DIR $SOURCE_URL
+# Create image (will take some time... go get a coffee)
+debootstrap $VARIANT_OPT $EXCLUDE_OPT $DISTRIB $DEST_DIR $SOURCE_URL
 
 # clean after debootstrap process (dpkg / apt)
 echo 
@@ -35,31 +34,41 @@ echo
 mount proc ${DEST_DIR}/proc -t proc
 mount sysfs ${DEST_DIR}/sys -t sysfs
 
+# Clean apt stuffs using apt inside rootdir
 chroot ${DEST_DIR} apt clean
-chroot ${DEST_DIR} rm -f /var/lib/apt/lists/httpredir.debian.org*
+# Wipe out the repo list archive(s)
+rm -f ${DEST_DIR}/var/lib/apt/lists/httpredir.debian.org*
 
 umount ${DEST_DIR}/sys
 umount ${DEST_DIR}/proc
 
 
 # New name for docker image
-echo "Wanna proceed with docker image import with name $DOCKER_USER/${VARIANT}_${DISTRIB} ?"
+echo "Do you want to proceed with docker image import with name $DOCKER_USER/${VARIANT}_${DISTRIB} ?"
 
 read resp
 echo
 
 case "$resp" in
 
-  "y"|"Y") 
+  "y"|"Y"|[Yy][Ee][Ss]) 
      echo "Ok. Let's do it."
-     tar -C $DEST_DIR -c . | docker import --message \"New base system from debootstrap variant $VARIANT\" - $DOCKER_USER/${VARIANT}_${DISTRIB}
+     tar -C $DEST_DIR -c . | docker import --message "New base system from debootstrap variant $VARIANT" - $DOCKER_USER/${VARIANT}_${DISTRIB}:${DATE_STAMP}
+     if [ "$?" -eq 0 ] ; then
+        echo "---"
+        echo "Now you can wipe out the $DEST_DIR source directory."
+        echo "---"
+        echo "Listing docker images:"
+        echo "---"
+     fi
      # list images
      docker images
   ;;
 
   *) 
      echo "Ok. You may copy and run the corresponding command below just in case:"
-     echo "tar -C $DEST_DIR -c . | docker import --message \"New base system from debootstrap variant $VARIANT\" - $DOCKER_USER/${VARIANT}_${DISTRIB}"
+     echo "tar -C $DEST_DIR -c . | docker import --message \"New base system from debootstrap variant $VARIANT\" - $DOCKER_USER/${VARIANT}_${DISTRIB}:${DATE_STAMP}"
   ;;
 
 esac
+
