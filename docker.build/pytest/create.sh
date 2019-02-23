549babbc75d3196ce86eeabf8a4b3cbf104979cf
Luiz Rogerio Carvalho <luizrogeriocarvalho@gmail.com>
2019-02-23 18:30:38 -0300

    Fixed pytest + pyinstaller to pytest instead of python.

---
+++ docker.build/pytest/create.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+WORKDIR="`dirname $0`"
+cd "$WORKDIR"
+
+# The generic definition
+[ -f ../generic.rc ] && . ../generic.rc
+
+# Folder is optional - end with a slash
+FOLDER=${BASE_FOLDER:-"lrgc01/"}
+# Versioning in order to keep a copy if running more than once
+# Note the ":" in the formated output
+# May change for own needs
+BUILD_VER=${GLOBAL_TAG_VER:-$(date +:%Y%m%d%H%M)}
+
+# Optionaly this script can prepare the docker-build environment
+if [ "$#" -gt 0 ]; then
+   case "$1" in
+      env|prepare|Dockerfile)
+        DOCKERFILE="Dockerfile"
+   ;;
+   esac
+else
+   DOCKERFILE="Dockerfile.tmp"
+fi
+
+COMMENT="Pytest n pyinstaller via pip3 over openssh-server image"
+IMGNAME="pytest_pyinstaller"
+
+UID_=${JENKINS_UID:-102}
+GID_=${JENKINS_GID:-103}
+USR_=${JENKINS_USR:-pyuser}
+GRP_=${JENKINS_GRP:-pygrp}
+USERDIR_=${JENKINS_HOMEDIR:-var/lib/jenkins}
+USERDIR_=${USERDIR_#/}
+
+START_CMD=${PY_START_CMD:-"python.start"}
+IPFILE=${PY_IPFILE:-"python.host"}
+
+# 
+# ---- Start command ----
+#    ... with workaround to get IP 
+#
+
+cat > $START_CMD << EOF
+#!/bin/bash
+
+# workaround to get my ip
+grep -w \$(hostname) /etc/hosts | awk '{print \$1}' > "/$USERDIR_/$IPFILE"
+# then start sshd
+/usr/sbin/sshd -D
+
+EOF
+# Seems useless, because when COPY by Dockerfile it looses file mode
+chmod 755 $START_CMD
+
+#
+# ---- end workaround IP ----
+
+cat > ${DOCKERFILE} << EOF
+#
+# This is a Dockerfile made from create.sh script - don't change here
+#
+FROM lrgc01/stretch_slim-ssh
+
+LABEL Comment="$COMMENT"
+
+COPY $START_CMD /
+
+RUN groupadd -g $GID_ $GRP_ && \\
+    useradd -M -u $UID_ -g $GRP_ -d /$USERDIR_ $USR_ && \\
+    set -ex && \\
+    apt-get update && \\
+    apt-get install -y python3-pip && \\
+    apt-get clean && \\
+    pip3 install pytest pyinstaller && \\
+    rm -f /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin && \\
+    rm -fr /var/lib/apt/lists/* && \\
+    rm -fr /usr/share/man/man* && \\
+    chmod 755 /$START_CMD
+
+EXPOSE 22
+
+CMD ["/$START_CMD"]
+EOF
+
+# Now build the image using docker build only if root is running
+if [ `whoami` = "root" ]; then
+  docker build -t ${FOLDER}${IMGNAME}${BUILD_VER} -f ${DOCKERFILE} .
+fi
+
+if [ "$DOCKERFILE" != "Dockerfile" ] ; then
+   # Cleaning only if Dockerfile.tmp is the current one
+   rm -fr ${OPTDIR} ${DOCKERFILE} $START_CMD
+fi
+
