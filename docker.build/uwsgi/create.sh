8e7f64fc792fb48d06d042a5ff8db9325b34782e
Luiz Rogerio Carvalho <luizrogeriocarvalho@gmail.com>
2019-08-09 14:18:26 -0300

    Some improvements in create.sh. Still TODO.

--- docker.build/uwsgi/create.sh
+++ docker.build/uwsgi/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WORKDIR="`dirname $0`"
-cd "$WORKDIR"
+BUILDDIR="`dirname $0`"
+cd "$BUILDDIR"
 
 # The generic definition
 [ -f ../generic.rc ] && . ../generic.rc
@@ -33,12 +33,15 @@ UID_=${UWSGI_UID:-10020}
 GID_=${UWSGI_GID:-10020}
 USR_=${UWSGI_USR:-uwsgi}
 GRP_=${UWSGI_GRP:-uwsgi}
-USERDIR_=${UWSGI_HOMEDIR:-uwsgi.d}
+STARTDIR_=${UWSGI_STARTDIR:-uwsgi.d}
+BASEDIR_=${UWSGI_BASEDIR:-uwsgi.d}
+BASEDIR_=${BASEDIR_#/}
+USERDIR_=${UWSGI_HOMEDIR:-$BASEDIR_/uwsgi}
 USERDIR_=${USERDIR_#/}
 
 START_CMD=${UWSGI_START_CMD:-"uwsgi.start"}
 UWSGI_APP=${UWSGI_UWSGI_APP:-"uwsgi_server.py"}
-APPDIR="appdir"
+APPDIR=${UWSGI_APPDIR:-"appdir"}
 UWSGI_LOG=${UWSGI_UWSGI_LOG:-"uwsgi.log"}
 UWSGI_INI=${UWSGI_UWSGI_INI:-"uwsgi.ini"}
 IPFILE=${UWSGI_IPFILE:-"uwsgi.host"}
@@ -50,13 +53,13 @@ cat > $UWSGI_INI << EOF
 [uwsgi]
 # Let's use 3 connection protocols
 http = :9090
-uwsgi-socket = /$USERDIR_/uwsgi.sock
-fastcgi-socket = /$USERDIR_/fastcgi.sock
+uwsgi-socket = /$BASEDIR_/uwsgi.sock
+fastcgi-socket = /$BASEDIR_/fastcgi.sock
 chmod-socket = 777
 # Could be a file only, but a complete app
 # in a defined path is more flexible
-#wsgi-file = /$USERDIR_/$APPDIR/$UWSGI_APP
-pythonpath = /$USERDIR_/$APPDIR
+#wsgi-file = /$BASEDIR_/$APPDIR/$UWSGI_APP
+pythonpath = /$BASEDIR_/$APPDIR
 wsgi = ${UWSGI_APP%.py}
 master = true
 processes = 4
@@ -77,11 +80,30 @@ EOF
 cat > $START_CMD << EOF
 #!/bin/bash
 
-# workaround to get my ip (head -1 = in case of duplicates)
-grep -w \$(hostname) /etc/hosts | head -1 | awk '{print \$1}' > "/$USERDIR_/$IPFILE"
+THIS="\$0"
+
+BASEDIR=\${DOCKER_BASEDIR:-"$BASEDIR_"}
+RUNUSER=\${DOCKER_RUNUSER:-"$USR_"}
+
+cd \$BASEDIR
+
+if [ \$(whoami)x = "rootx" ] ; then
+   env > environment.rc
+   su -l \$RUNUSER -s /bin/bash -c "sh \$THIS \$*"
+   exit 0
+fi
+
+#set -x
+
+[ -f "environment.rc" ] && . environment.rc
+export HOME=\$BASEDIR
+export WORKDIR=\$BASEDIR
+
 
 #
 # Receive (or not) some env variables from Docker container
+# All of them start with DOCKER_
+# The "UWSGI" ones go into INI file
 #
 START_CMD=\${DOCKER_START_CMD:-"$START_CMD"}
 UWSGI_LOG=\${DOCKER_UWSGI_LOG:-"$UWSGI_LOG"}
@@ -92,25 +114,31 @@ UWSGI_INI=\${DOCKER_UWSGI_INI:-"$UWSGI_INI"}
 UWSGI_PROCESSES=\${DOCKER_UWSGI_PROCESSES:-"4"}
 UWSGI_THREADS=\${DOCKER_UWSGI_THREADS:-"2"}
 UWSGI_STATS=\${DOCKER_UWSGI_STATS:-"0.0.0.0:9191"}
-USERDIR_=\${DOCKER_UWSGI_USERDIR:-"$USERDIR_"}
+USERDIR=\${DOCKER_USERDIR:-"$USERDIR_"}
+CONFIGDIR=\${DOCKER_CONFIGDIR:-"$BASEDIR_"}
+LOGDIR=\${DOCKER_LOGDIR:-"$BASEDIR_"}
 USR_=\${DOCKER_UWSGI_USR:-"$USR_"}
 GRP_=\${DOCKER_UWSGI_GRP:-"$GRP_"}
+
+
+# Make sure every directory is ready to do the job
+mkdir -p \$USERDIR \$CONFIGDIR \$LOGDIR > /dev/null 2>&1
 
 #
 # ---- Build INI file ----
 #
 
-cat > \$UWSGI_INI << ENDOF
+cat > \$CONFIGDIR/\$UWSGI_INI << ENDOF
 [uwsgi]
 # Let's use 3 connection protocols
 http = \$UWSGI_HTTP
-uwsgi-socket = /\$USERDIR_/uwsgi.sock
-fastcgi-socket = /\$USERDIR_/fastcgi.sock
+uwsgi-socket = \$BASEDIR/uwsgi.sock
+fastcgi-socket = \$BASEDIR/fastcgi.sock
 chmod-socket = 777
 # Could be a file only, but a complete app
 # in a defined path is more flexible
-#wsgi-file = /\$USERDIR_/\$UWSGI_APPDIR/\$UWSGI_APP
-pythonpath = /\$USERDIR_/\$UWSGI_APPDIR
+#wsgi-file = \$BASEDIR/\$UWSGI_APPDIR/\$UWSGI_APP
+pythonpath = \$BASEDIR/\$UWSGI_APPDIR
 wsgi = \${UWSGI_APP%.py}
 master = true
 processes = \$UWSGI_PROCESSES
@@ -122,8 +150,11 @@ ENDOF
 #
 # ---- end INI file ----
 
-# Start the uWSGI server (might change the /\$USERDIR_/\$UWSGI_INI file to fit your needs)
-/usr/local/bin/uwsgi /\$USERDIR_/\$UWSGI_INI --daemonize2 /\$USERDIR_/\$UWSGI_LOG
+# Start the uWSGI server (might change the /\$CONFIGDIR/\$UWSGI_INI file to fit your needs)
+/usr/local/bin/uwsgi \$CONFIGDIR/\$UWSGI_INI --daemonize2 \$LOGDIR/\$UWSGI_LOG
+
+# workaround to get my ip (head -1 = in case of duplicates)
+grep -w \$(hostname) /etc/hosts | head -1 | awk '{print \$1}' > "\$BASEDIR/$IPFILE"
 
 # then start sshd
 /usr/sbin/sshd -D
@@ -156,7 +187,7 @@ LABEL Comment="$COMMENT"
 
 RUN groupadd -g $GID_ $GRP_ && \\
     useradd -m -u $UID_ -g $GRP_ -d /$USERDIR_ $USR_ && \\
-    mkdir -p /$USERDIR_/$APPDIR && chown -R $UID_:$GID_ /$USERDIR_/$APPDIR && \\
+    mkdir -p /$BASEDIR_/$APPDIR && chown -R $UID_:$GID_ /$BASEDIR_ && \\
     set -ex && \\
     pip install uwsgi && \\
     rm -f /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin && \\
@@ -164,17 +195,17 @@ RUN groupadd -g $GID_ $GRP_ && \\
     rm -fr /usr/share/man/man* 
 
 # After creating user homedir
-COPY --chown=$UID_:$GID_ $START_CMD $UWSGI_INI /$USERDIR_/
-COPY --chown=$UID_:$GID_ $UWSGI_APP /$USERDIR_/$APPDIR/
+COPY --chown=$UID_:$GID_ $START_CMD $UWSGI_INI /$BASEDIR_/
+COPY --chown=$UID_:$GID_ $UWSGI_APP /$BASEDIR_/$APPDIR/
 
-VOLUME ["/$USERDIR_"]
+VOLUME ["/$BASEDIR_"]
 
 
 # 9090 listen for connections / 9191 monitoring
 EXPOSE 9090 
 EXPOSE 9191
 
-CMD ["sh","/$USERDIR_/$START_CMD"]
+CMD ["sh","/$BASEDIR_/$START_CMD"]
 EOF
 
 # Now build the image using docker build only if root is running
