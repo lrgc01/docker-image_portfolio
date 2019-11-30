e4e0c6d2c2e0ef838eb262ba960a69e4b8286c0c
Luiz Rogerio Carvalho <luizrogeriocarvalho@gmail.com>
2019-11-30 18:00:16 -0300

    Added ruby similar to nodejs approach.

---
+++ docker.build/ruby/create.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+WORKDIR="`dirname $0`"
+cd "$WORKDIR"
+
+# The generic definition
+[ -f ../generic.rc ] && . ../generic.rc
+
+# Folder is optional - end with a slash
+FOLDER=${BASE_FOLDER:-"lrgc01/"}
+# Versioning in order to keep a copy if running more than once
+# Note the ":" in the formated output
+# May change for own needs
+BUILD_VER=${GLOBAL_TAG_VER:-$(date +:%Y%m%d%H%M)}
+
+# Optionaly this script can prepare the docker-build environment
+#
+##### WARNING
+#####
+##### this Dockerfile is built from nodejs official Docker
+##### Don't overwrite here. Using .tmp to the future.
+if [ "$#" -gt 0 ]; then
+   case "$1" in
+      env|prepare|Dockerfile)
+	BUILD_ENV="1"
+        DOCKERFILE="Dockerfile.tmp"
+   ;;
+   esac
+else
+   DOCKERFILE="Dockerfile.tmp"
+fi
+
+COMMENT="Ruby over openssh-server image"
+IMGNAME="ruby-strech_slim"
+
+UID_=${JENKINS_UID:-102}
+GID_=${JENKINS_GID:-103}
+USR_=${JENKINS_USR:-rbuser}
+GRP_=${JENKINS_GRP:-rbgrp}
+USERDIR_=${JENKINS_HOMEDIR:-var/lib/jenkins}
+USERDIR_=${USERDIR_#/}
+
+START_CMD=${RUBY_START_CMD:-"ruby.start"}
+IPFILE=${RUBY_IPFILE:-"ruby.host"}
+
+# 
+# ---- Start command ----
+#    ... with workaround to get IP 
+#
+
+cat > $START_CMD << EOF
+#!/bin/bash
+
+# workaround to get my ip
+grep -w \$(hostname) /etc/hosts | awk '{print \$1}' > "/$USERDIR_/$IPFILE"
+# then start sshd
+/usr/sbin/sshd -D
+
+EOF
+# Seems useless, because when COPY by Dockerfile it looses file mode
+chmod 755 $START_CMD
+
+#
+# ---- end workaround IP ----
+
+cat > ${DOCKERFILE} << EOF
+#
+# This is a Dockerfile made from create.sh script - don't change here
+#
+FROM lrgc01/ssh-stretch_slim
+
+LABEL Comment="$COMMENT"
+
+COPY $START_CMD /
+
+RUN groupadd -g $GID_ $GRP_ && \\
+    useradd -M -u $UID_ -g $GRP_ -d /$USERDIR_ $USR_ && \\
+    set -ex && \\
+    apt-get update && \\
+    apt-get install -y python3-pip && \\
+    apt-get clean && \\
+    pip3 install pytest pyinstaller && \\
+    rm -f /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin && \\
+    rm -fr /var/lib/apt/lists/* && \\
+    rm -fr /usr/share/man/man* && \\
+    chmod 755 /$START_CMD
+
+EXPOSE 22
+
+CMD ["/$START_CMD"]
+EOF
+
+# Now build the image using docker build only if root is running
+if [ `whoami` = "root" -a "$BUILD_ENV" != "1" ]; then
+  #docker build -t ${FOLDER}${IMGNAME}${BUILD_VER} -f ${DOCKERFILE} .
+  docker build -t ${FOLDER}${IMGNAME}${BUILD_VER} -f Dockerfile .
+fi
+
+if [ "$DOCKERFILE" != "Dockerfile"  -a $BUILD_ENV != "1" ] ; then
+   # Cleaning only if Dockerfile.tmp is the current one
+   rm -fr ${OPTDIR} ${DOCKERFILE} $START_CMD
+fi
+
