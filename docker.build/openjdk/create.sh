a62f7c696ab40fcabbcef91989095b4fb260047f
Luiz Rogerio <luizrogeriocarvalho@gmail.com>
2020-05-30 19:17:32 -0300

    Upgraded openjdk to auto build in hub.docker.com

--- docker.build/openjdk/create.sh
+++ docker.build/openjdk/create.sh
@@ -3,33 +3,68 @@
 WORKDIR="`dirname $0`"
 cd "$WORKDIR"
 
-COMMENT="Adding jdk on top of base image"
+# The generic definition
+[ -f ../generic.rc ] && . ../generic.rc
 
 # Folder is optional - end with a slash
-FOLDER="lrgc01/"
-IMGNAME="openjdk"
+FOLDER=${BASE_FOLDER:-"lrgc01/"}
 # Versioning in order to keep a copy if running more than once
 # Note the ":" in the formated output
 # May change for own needs
-BUILD_VER=$(date +:%Y%m%d%H%M)
+BUILD_VER=${GLOBAL_TAG_VER:-$(date +:%Y%m%d%H%M)}
 
-DOCKERFILE="Dockerfile.tmp"
+# Optionaly this script can prepare the docker-build environment
+if [ "$#" -gt 0 ]; then
+	   case "$1" in
+      env|prepare|Dockerfile)
+        BUILD_ENV="1"
+        DOCKERFILE="Dockerfile"
+   ;;
+   esac
+else
+   DOCKERFILE="Dockerfile.tmp"
+fi
 
+COMMENT="Adding jdk on top of base image"
+
+# Folder is optional - end with a slash
+IMGNAME="openjdk"
+FROMIMG="lrgc01/ssh-debian_slim"
+
+# 
+# Dockerfile
+#
 cat > ${DOCKERFILE} << EOF
-FROM lrgc01/minbase_stable_ssh
+FROM $FROMIMG
+
 LABEL Comment="$COMMENT"
-RUN apt-get update && \
-    apt-get install -y default-jdk-headless && \
-    apt-get clean && \
-    rm -f /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin && \
-    rm -f /var/lib/apt/lists/*debian.org* && \
-    rm -fr /usr/share/man/man* 
+
+# Workaround to avoid install error when linking man page
+RUN apt-get update && \\
+    mkdir -p /usr/share/man/man1 ; \\
+    apt-get install -y default-jdk-headless && \\
+    apt-get clean && \\
+    rm -f /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin && \\
+    rm -f /var/lib/apt/lists/*debian.org* && \\
+    rm -fr /usr/share/man/man*/*
+
 VOLUME ["/usr/lib/jvm"]
-CMD ["/bin/bash"]
+
+EXPOSE 22
+
+CMD ["/usr/sbin/sshd","-D"]
 EOF
 
-# Now build the image using docker build
-docker build -t ${FOLDER}${IMGNAME}${BUILD_VER} -f ${DOCKERFILE} .
+# Now build the image using docker build only if root is running
+if [ `whoami` = "root" -a "$BUILD_ENV" != "1" ]; then
+	  docker build -t ${FOLDER}${IMGNAME}${BUILD_VER} -f ${DOCKERFILE} .
+fi
+#
+# ---- end docker build ----
 
 # Cleaning
-rm -fr ${DOCKERFILE}
+if [ "$DOCKERFILE" != "Dockerfile" ] ; then
+    # Cleaning only if Dockerfile.tmp is the current one
+    rm -fr ${OPTDIR} ${DOCKERFILE} ${TAR_BALL} "$USERDIR_" usr var etc
+fi
+
