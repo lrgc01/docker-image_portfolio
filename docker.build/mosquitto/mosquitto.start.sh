03e6346644f005f84e078e0e0cd550e59c4d8fb1
Luiz Rogerio <luizrogeriocarvalho@gmail.com>
2022-06-20 17:16:59 -0300

    mosquitto: New start script

--- docker.build/mosquitto/mosquitto.start.sh
+++ docker.build/mosquitto/mosquitto.start.sh
@@ -2,26 +2,37 @@
 
 ETCD="/etc/mosquitto"
 CERTDIR="$ETCD/certs"
+CACERTDIR="$ETCD/ca_certificates"
 PASSWDFILE="$ETCD/authorization_file" # use mosquitto_passwd to create one
-SOCKETCONF="$ETCD/conf.d/socket_domain.conf"
-SSLCONF="$ETCD/conf.d/ssl.conf"
-AUTHCONF="$ETCD/conf.d/auth.conf"
+LISTENERCONF="$ETCD/conf.d/listener.conf"
 
 if [ ! -d "/run/mosquitto" ] ; then
    mkdir -p /run/mosquitto 
 fi
 chown mosquitto /run/mosquitto
 
-if [ ! -f "$SOCKETCONF" ] ; then
-   echo "socket_domain ipv4" > $SOCKETCONF
-   echo "listener 8883" >> $SOCKETCONF # 8883 means use CA/CERTS provided by OS
+if [ ! -f "$LISTENERCONF" ] ; then
+   cat > $LISTENERCONF << EOF
+# MQTT, no encrypt
+listener 1883
+allow_anonymous false
+set_tcp_nodelay true
+
+# MQTT, encrypt
+listener 8883
+allow_anonymous false
+set_tcp_nodelay true
+require_certificate false
+cafile $CACERTDIR/ca.crt
+certfile $CERTDIR/server.crt
+keyfile $CERTDIR/server.key
+password_file /etc/mosquitto/authorization_file
+EOF
 fi
 
-if [ ! -f "$SSLCONF" ] ; then
-   echo "require_certificate false" >> $SSLCONF
-fi
-if [ ! -f "$AUTHCONF" ] ; then
-   echo "password_file /etc/mosquitto/authorization_file" > $AUTHCONF
+if [ ! -f "$PASSWDFILE" ]; then
+   touch $PASSWDFILE
+   chown mosquitto:mosquitto $PASSWDFILE
 fi
 
 chown -R mosquitto /etc/mosquitto
