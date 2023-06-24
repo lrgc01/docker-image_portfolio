2f89436d657a344e4ef1a1c8aa5809c794cf4b9b
Luiz Rogerio <luizrogeriocarvalho@gmail.com>
2023-06-24 22:26:36 +0000

    openjdk: at work: creating a global create.sh script

--- docker.build/openjdk/create.sh
+++ docker.build/openjdk/create.sh
@@ -3,15 +3,14 @@
 WORKDIR="`dirname $0`"
 cd "$WORKDIR"
 
-# The generic definition
-[ -f ../generic.rc ] && . ../generic.rc
+# The generic and then local definition
+for RCFILE in "../generic.rc" "./local.rc"
+do
+   [ -f "$RCFILE" ] && . "$RCFILE"
+done
 
 # Folder is optional - end with a slash
 FOLDER=${BASE_FOLDER:-"lrgc01/"}
-# Versioning in order to keep a copy if running more than once
-# Note the ":" in the formated output
-# May change for own needs
-BUILD_VER=${GLOBAL_TAG_VER:-$(date +:%Y%m%d%H%M)}
 
 # Optionaly this script can prepare the docker-build environment
 if [ "$#" -gt 0 ]; then
@@ -25,35 +24,10 @@ else
    DOCKERFILE="Dockerfile.tmp"
 fi
 
-COMMENT="Adding jdk on top of base image"
-
-# Folder is optional - end with a slash
-IMGNAME="openjdk"
-FROMIMG="lrgc01/ssh-debian_slim"
-
-# 
-# Dockerfile
-#
-cat > ${DOCKERFILE} << EOF
-FROM $FROMIMG
-
-LABEL Comment="$COMMENT"
-
-# Workaround to avoid install error when linking man page
-RUN apt-get update && \\
-    mkdir -p /usr/share/man/man1 ; \\
-    apt-get install -y default-jdk-headless && \\
-    apt-get clean && \\
-    rm -f /var/cache/apt/pkgcache.bin /var/cache/apt/srcpkgcache.bin && \\
-    rm -f /var/lib/apt/lists/*debian.org* && \\
-    rm -fr /usr/share/man/man*/*
-
-VOLUME ["/usr/lib/jvm"]
-
-EXPOSE 22
-
-CMD ["/usr/sbin/sshd","-D"]
-EOF
+# Comes from local.rc definition
+if [ ! -z "$_DOCKERBODY" ]; then
+   echo "$_DOCKERBODY" > $DOCKERFILE
+fi
 
 # Now build the image using docker build only if root is running
 if [ `whoami` = "root" -a "$BUILD_ENV" != "1" ]; then
