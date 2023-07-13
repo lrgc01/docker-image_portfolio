a6addf612e7bc2e566fe70d691bcfdd79513b69c
Luiz Rogerio <luizrogeriocarvalho@gmail.com>
2023-07-13 12:32:35 -0300

    open JRE: new runtime set of files based on jdk

--- docker.build/openjre/create.sh
+++ docker.build/openjre/create.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-WORKDIR="`dirname $0`"
-cd "$WORKDIR"
-
-# Folder is optional - end with a slash
-FOLDER="lrgc01/"
-IMGNAME="jre"
-# Versioning in order to keep a copy if running more than once
-# Note the ":" in the formated output
-# May change for own needs
-BUILD_VER=$(date +:%Y%m%d%H%M)
-
-# Now build the image using docker build
-docker build -t ${FOLDER}${IMGNAME}${BUILD_VER} .
-
+../scripts/create.sh
