b7d982de138d63a27b8539b506580bf905f211c1
Luiz Rogerio <luizrogeriocarvalho@gmail.com>
2023-10-23 19:09:13 -0300

    Added php container to periodic tasks

--- docker.build/scripts/periodic.sh
+++ docker.build/scripts/periodic.sh
@@ -36,7 +36,7 @@ if [ $(whoami) != "root" ]; then
 fi
 
 # Order is VERY important here
-BUILDLIST="ssh-stable_slim net-stable_slim dns-bind9 python3-pip nginx mariadb apache2 samba git nvm openjre openjdk debian-nox11"
+BUILDLIST="ssh-stable_slim net-stable_slim dns-bind9 python3-pip nginx mariadb apache2 samba git nvm php openjre openjdk debian-nox11"
 
 for bld in $BUILDLIST
 do
